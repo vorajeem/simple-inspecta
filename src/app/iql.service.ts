64e75ecbdcb98ef90f6bd3ae7928648550f05b03
vorajeem <vorajeem@gmail.com>
2018-09-06 12:40:02 +0200

    added some connect() to IQL service

--- src/app/iql.service.ts
+++ src/app/iql.service.ts
@@ -5,5 +5,21 @@ import { Injectable } from '@angular/core';
 })
 export class IqlService {
 
+  private connection;
+  private isConnected = false;
   constructor() { }
+
+  public connect() {
+    const host = window.location.host.substring(0,
+    window.location.host.indexOf(':')) + ':8080';
+
+    const wsurl = 'ws://' + host + '/websockets/iql.js';
+
+    this.connection = new WebSocket(wsurl);
+
+    this.connection.onopen = (event) => {
+      this.isConnected = true;
+      console.log('Connected ekse');
+    };
+  }
 }
