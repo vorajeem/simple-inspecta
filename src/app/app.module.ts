44b63266ff4b0ae26ebd7876521e9f36690051fd
vorajeem <vorajeem@gmail.com>
2018-09-06 12:27:24 +0200

    added IQL services to providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [IqlService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
